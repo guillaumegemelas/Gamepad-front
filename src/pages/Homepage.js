271d59cf05621e3cb5a0829b38693cecd946354e
Guillaume Gemelas <GuiGui@MacBook-Pro.local>
2023-02-27 22:40:36 +0100

    test affichage données

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -13,9 +13,10 @@ const Homepage = () => {
           "https://api.rawg.io/api/games?key=b144d325b8cd4cee8a7ad6c204cab7d2"
         );
         setGames(response.data);
-        console.log(response.data.results[0]);
+        console.log(response.data.results[0], "response data result[0]");
+        console.log(response.data, "response data");
         //renvoie les résultats de la requete à l'API
-        setIsLoading(true);
+        setIsLoading(false);
       } catch (error) {
         console.log(error.message);
         console.log(error.response);
@@ -28,7 +29,28 @@ const Homepage = () => {
   return (
     <div>
       <div>Vous êtes sur la Homepage</div>
-      <div></div>
+      {isLoading ? (
+        <div>
+          <p>En cours de chargement...</p>
+        </div>
+      ) : (
+        <div>
+          <p>page chargée</p>
+          {games.results.map((elem, index) => {
+            return (
+              <div key={index}>
+                <h1>{elem.name}</h1>
+                <h1>{elem.rating}</h1>
+                <img
+                  style={{ width: 100, height: 100 }}
+                  src={elem.background_image}
+                  alt=""
+                />
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
