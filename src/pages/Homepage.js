42838b0b50f2fb222a650fcb6ae587585429d5c0
Guillaume Gemelas <GuiGui@MacBook-Pro.local>
2023-02-28 13:46:26 +0100

    ajout searchbar

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,16 +1,19 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 
+import logo from "../img/logo.jpg";
+
 //page principal sur la quelle apparaissent tous les jeux issu de l'API
 const Homepage = () => {
   const [games, setGames] = useState();
   const [isLoading, setIsLoading] = useState(true);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          "https://api.rawg.io/api/games?key=b144d325b8cd4cee8a7ad6c204cab7d2"
+          `https://api.rawg.io/api/games?key=b144d325b8cd4cee8a7ad6c204cab7d2&search=${search}`
         );
         setGames(response.data);
         console.log(response.data.results[0], "response data result[0]");
@@ -23,19 +26,32 @@ const Homepage = () => {
       }
     };
     fetchData();
-  }, []);
+  }, [search]);
 
   // j'ai bien une réponse du serveur avec tous les jeux
   return (
     <div>
-      <div>Vous êtes sur la Homepage</div>
       {isLoading ? (
         <div>
           <p>En cours de chargement...</p>
         </div>
       ) : (
         <div>
-          <p>page chargée</p>
+          {/* searchbar: filtre par nom -----------------------------------------------------------*/}
+          <section className="searchBar">
+            <div className="homePageLogo">
+              <img src={logo} alt="logo GamePad" />
+            </div>
+            <input
+              className="search"
+              value={search}
+              type="text"
+              placeholder="Search for a game..."
+              onChange={(event) => setSearch(event.target.value)}
+            />
+          </section>
+
+          {/* reponse serveur: games du moment -----------------------------------------------------------*/}
           {games.results.map((elem, index) => {
             return (
               <div key={index}>
