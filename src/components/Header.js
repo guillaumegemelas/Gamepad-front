d1b831c843007c14cbc1d6ed1f42554c5ab37aeb
Guillaume Gemelas <GuiGui@MacBook-Pro.local>
2023-03-01 17:07:22 +0100

    improve css1

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,12 @@ const Header = () => {
       <div className="logo">
         <img src={logo1} alt="logo GamePad" />
       </div>
+      <div className="powered">
+        <p>
+          Powered by{" "}
+          <span style={{ textDecoration: "underline" }}>Rawg API</span>{" "}
+        </p>
+      </div>
     </div>
   );
 };
