5dd2567db3e130fc95f744625a732d69a3736471
Guillaume Gemelas <guillaumegemelas@hotmail.com>
2023-05-16 16:19:46 +0200

    add class component homefirst and gamecard

--- src/components/Gamecard/GameCard.js
+++ src/components/Gamecard/GameCard.js
@@ -1,28 +1,62 @@
 import { Link } from "react-router-dom";
 
+//import pour les composants class
+import { Component } from "react";
+
 //import style.css
 import "./style.css";
 
-const GameCard = ({ games }) => {
-  return (
-    <div className="gamesResult">
-      {games.results.map((elem) => {
-        return (
-          <section key={elem.id}>
-            <Link className="gameCard" to={`/game/${elem.id}`}>
-              <div className="hoverCard">
-                <div id="slider" className="carousel">
-                  <img src={elem.background_image} alt="picture1" />
-                </div>
-              </div>
+//composant class au lieu de composant fonctionnel
+class GameCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-              <h1>{elem.name}</h1>
-            </Link>
-          </section>
-        );
-      })}
-    </div>
-  );
-};
+  render() {
+    const { games } = this.props;
+    return (
+      <div className="gamesResult">
+        {games.results.map((elem) => {
+          return (
+            <section key={elem.id}>
+              <Link className="gameCard" to={`/game/${elem.id}`}>
+                <div className="hoverCard">
+                  <div id="slider" className="carousel">
+                    <img src={elem.background_image} alt="picture1" />
+                  </div>
+                </div>
+
+                <h1>{elem.name}</h1>
+              </Link>
+            </section>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+// const GameCard = ({ games }) => {
+//   return (
+//     <div className="gamesResult">
+//       {games.results.map((elem) => {
+//         return (
+//           <section key={elem.id}>
+//             <Link className="gameCard" to={`/game/${elem.id}`}>
+//               <div className="hoverCard">
+//                 <div id="slider" className="carousel">
+//                   <img src={elem.background_image} alt="picture1" />
+//                 </div>
+//               </div>
+
+//               <h1>{elem.name}</h1>
+//             </Link>
+//           </section>
+//         );
+//       })}
+//     </div>
+//   );
+// };
 
 export default GameCard;
--- src/pages/Homefirst/index.js
+++ src/pages/Homefirst/index.js
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
 
+//import pour composant class
+import { Component } from "react";
+
 //import style.css
 import "./style.css";
 
@@ -9,23 +12,44 @@ import { Fade } from "react-awesome-reveal";
 //import icones
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Home = () => {
-  return (
-    <div className="HomeFirst">
-      <Fade cascade damping={1}>
-        {" "}
-        {/* <img src={kratos} alt="kratosImg" /> */}
-        <Link to={`/games`}>
-          <div className="buttonHome">
-            <div className="icon">
-              {/* <FontAwesomeIcon icon="right-to-bracket" /> */}
-              <FontAwesomeIcon icon="door-open" />
+class Home extends Component {
+  render() {
+    return (
+      <div className="HomeFirst">
+        <Fade cascade damping={1}>
+          {" "}
+          {/* <img src={kratos} alt="kratosImg" /> */}
+          <Link to={`/games`}>
+            <div className="buttonHome">
+              <div className="icon">
+                {/* <FontAwesomeIcon icon="right-to-bracket" /> */}
+                <FontAwesomeIcon icon="door-open" />
+              </div>
             </div>
-          </div>
-        </Link>
-      </Fade>
-    </div>
-  );
-};
+          </Link>
+        </Fade>
+      </div>
+    );
+  }
+}
+
+// const Home = () => {
+//   return (
+//     <div className="HomeFirst">
+//       <Fade cascade damping={1}>
+//         {" "}
+//         {/* <img src={kratos} alt="kratosImg" /> */}
+//         <Link to={`/games`}>
+//           <div className="buttonHome">
+//             <div className="icon">
+//               {/* <FontAwesomeIcon icon="right-to-bracket" /> */}
+//               <FontAwesomeIcon icon="door-open" />
+//             </div>
+//           </div>
+//         </Link>
+//       </Fade>
+//     </div>
+//   );
+// };
 
 export default Home;
